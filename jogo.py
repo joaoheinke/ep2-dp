d6121caa9d80578d75fa2b3552e2e942702c7324
joaoheinke <joaovtito@gmail.com>
2023-01-24 14:35:33 -0300

    segunda função

--- jogo.py
+++ jogo.py
@@ -9,3 +9,12 @@ def define_posicoes(linha,coluna,orientacao,tamanho):
             posicoes.append([linha+i,coluna])
         i+=1
     return posicoes
+
+def preenche_frota(frota,nome,linha,coluna,orientacao,tamanho):
+    if nome not in frota:
+        frota[nome]=[define_posicoes(linha,coluna,orientacao,tamanho)]
+
+    else:
+        frota[nome].append(define_posicoes(linha,coluna,orientacao,tamanho))
+        
+    return frota
