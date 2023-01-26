37c0c4f8c7687a4dd49a32360ad327990c9687ac
Leonardo Ponciano <leonardops3@al.insper.edu.br>
2023-01-26 10:42:09 -0300

    funcao posiciona_frota

--- funcoes.py
+++ funcoes.py
@@ -26,3 +26,24 @@ def faz_jogada(tabuleiro,linha,coluna):
     if tabuleiro[linha][coluna] == 0:
         tabuleiro[linha][coluna] = '-'
     return tabuleiro
+
+def posiciona_frota(dic):
+    lista=[
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+    for navio, listap in dic.items():
+        for posicoes in listap:
+            for posicao in posicoes:
+                x=posicao[0]
+                y=posicao[1]
+                lista[x][y]=1
+    return lista
